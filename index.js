d9ba1e75225bdd5c12fd422827590a5a5bb1139d
Andreas Deuschlinger <andreas.deuschlinger@gmail.com>
2016-02-19 21:39:09 +0100

    added jsdoc doclets

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ var config = require('./loadConfig')(gulp, $);
 /**
  * GulpPilot helps you to manage you build tasks in separate, well structured files.
  *
- * @constructor
+ * **Peer-Dependencies:** This plugins requires your package to use gulp, gulp-util and gulp-load-plugins.
+ * @constructor GulpPilot
  */
 function GulpPilot() {}
 
@@ -20,7 +21,48 @@ GulpPilot.prototype = {
     get: get
 };
 
-// get task function
+/**
+ * The complete name of the task. Subtasks are separated by a colon (:).
+ * @typedef {string} TaskToken
+ * @example
+ * 'foo'
+ * 'foo:sub'
+ * 'foo:sub1:sub2'
+ *
+ * 'foo/bar'
+ * 'foo/bar:sub'
+ */
+
+/**
+ * Will add a new gulp task by grabbing the task's function implementation
+ * automatically from a JS file with the gulp/ folder.
+ *
+ * @memberOf GulpPilot#
+ * @param {TaskToken} name - The name of the task.
+ * @param {Array} [dependencies] - An array of task names to be executed and completed before your task will run.
+ * @returns {GulpPilot} Returns itself to enable method chaining.
+ * @example
+ * // example folder structure:
+ * //
+ * // |-dist/
+ * // |-node_modules/
+ * // |-src/
+ * // |  |-...
+ * // |-gulp/
+ * // |  |- foo.js
+ * // |  |- bar.js
+ * // |-gulpfile.js
+ * // |-package.json
+ * // |-README.md
+ *
+ * // in your gulpfile.js
+ * var pilot = require('gulp-pilot');
+ *
+ * // will load from gulp/foo.js
+ * pilot.task('foo');
+ * // will load from gulp/bar.js with dependency 'foo'
+ * pilot.task('bar', ['foo']);
+ */
 function task(name, dependencies) {
     var task = get(name);
 
@@ -35,6 +77,35 @@ function task(name, dependencies) {
     return this;
 }
 
+/**
+ * Get a task' function implementation by name.
+ *
+ * @memberOf GulpPilot#
+ * @param {TaskToken} name - The name of the task.
+ * @returns {Function} Returns the function that implements the task.
+ * @example
+ * // example folder structure:
+ * //
+ * // |-dist/
+ * // |-node_modules/
+ * // |-src/
+ * // |  |-...
+ * // |-gulp/
+ * // |  |- foo.js
+ * // |  |- bar.js
+ * // |-gulpfile.js
+ * // |-package.json
+ * // |-README.md
+ *
+ * // in your gulpfile.js
+ * var gulp = require('gulp');
+ * var pilot = require('gulp-pilot');
+ *
+ * // will load from gulp/foo.js
+ * gulp.task('foo', pilot.get('foo'));
+ * // will load from gulp/bar.js with dependency 'foo'
+ * gulp.task('bar', ['foo'], pilot.get('foo'));
+ */
 function get(name) {
     var log = $.util.log;
     var chalk = $.util.colors;
